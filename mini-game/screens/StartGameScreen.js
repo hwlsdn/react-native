05cdcf183805b78777cbcaedfe4a62b0a808024b
Jin Woo Han <jin00woo@naver.com>
2023-05-13 15:35:42 +0900

    Added two buttons and text input area.

--- mini-game/screens/StartGameScreen.js
+++ mini-game/screens/StartGameScreen.js
@@ -1,15 +1,61 @@
-import { TextInput, View } from 'react-native';
+import { TextInput, View, StyleSheet } from "react-native";
 
-import PrimaryButton from '../components/PrimaryButton';
+import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
-    </View>
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View> 
   );
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    borderRadius: 8,
+    backgroundColor: "#4e0329",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 4 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    textAlign: "center",
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
