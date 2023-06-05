8b310729a6b3e113d672d4b5ae49884a51182654
Jin Woo Han <jin00woo@naver.com>
2023-06-05 19:19:45 +0900

    Implemented the same logic using redux.

--- food-app/App.js
+++ food-app/App.js
@@ -9,6 +9,8 @@ import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetailScreen from "./screens/MealDetailScreen";
 import FavoritesScreen from "./screens/FavoritesScreen";
 import FavoritesContextProvider from "./store/context/favorites-context";
+import { Provider } from "react-redux";
+import { store } from "./store/redux/store";
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
@@ -28,7 +30,7 @@ function DrawerNavigator() {
 
 export default function App() {
   return (
-    <FavoritesContextProvider>
+    <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
@@ -40,7 +42,7 @@ export default function App() {
           <Stack.Screen name="MealDetail" component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
-    </FavoritesContextProvider>
+    </Provider>
   );
 }
 
