4a2b0136b0da8a76bb0de6020a76c2fb92749cd9
Jin Woo Han <jin00woo@naver.com>
2023-06-10 15:48:06 +0900

    Finished implementing image picker component.

--- photos-app/components/Places/ImagePicker.js
+++ photos-app/components/Places/ImagePicker.js
@@ -1,11 +1,15 @@
-import { View, Button, Alert } from "react-native";
+import { View, Alert, Image, StyleSheet, Text } from "react-native";
 import {
   launchCameraAsync,
   useCameraPermissions,
   PermissionStatus,
 } from "expo-image-picker";
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
+import OutlinedButton from "../UI/OutlinedButton";
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
@@ -35,13 +39,39 @@ function ImagePicker() {
       aspect: [16, 9],
       quality: 0.5,
     });
+    setPickedImage(image.assets[0].uri);
   }
+
+  let imagePreview = <Text>No image picked yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image style={styles.image} source={{ uri: pickedImage }} />;
+  }
+
   return (
     <View>
-      <View></View>
-      <Button title="Take Image" onPress={takeImageHandler} />
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <OutlinedButton icon="camera" onPress={takeImageHandler}>
+        Take Photo
+      </OutlinedButton>
     </View>
   );
 }
 
 export default ImagePicker;
+
+const styles = StyleSheet.create({
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginVertical: 8,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.primary100,
+    borderRadius: 4,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
