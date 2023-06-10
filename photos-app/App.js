4cc5367496f2ede3b4f9d278cea120a9f949e920
Jin Woo Han <jin00woo@naver.com>
2023-06-10 19:21:05 +0900

    Started creating location picker component.

--- photos-app/App.js
+++ photos-app/App.js
@@ -5,6 +5,7 @@ import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
 import IconButton from "./components/UI/IconButton";
 import { Colors } from "./constants/colors";
+import Map from "./screens/Map";
 
 const Stack = createNativeStackNavigator();
 
@@ -40,6 +41,7 @@ export default function App() {
             component={AddPlace}
             options={{ title: "Add a New Place" }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
