cc8d2a75f1602aa36f8c915931d32410f9c4c970
Jin Woo Han <jin00woo@naver.com>
2023-06-11 16:32:55 +0900

    Finished Fetch, Create, Load logic of db.

--- photos-app/screens/AllPlaces.js
+++ photos-app/screens/AllPlaces.js
@@ -7,10 +7,15 @@ function AllPlaces({ route }) {
 
   const isFocused = useIsFocused();
   useEffect(() => {
-    if (isFocused && route.params) {
-      setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    async function loadPlaces() {
+      const places = await fetchPlaces();
+      setLoadedPlaces(places);
     }
-  }, [isFocused, route]);
+    if (isFocused) {
+      loadPlaces();
+      //   setLoadedPlaces((curPlaces) => [...curPlaces, route.params.place]);
+    }
+  }, [isFocused]);
   return <PlacesList places={loadedPlaces} />;
 }
 
