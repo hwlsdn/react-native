f0f8bf0684ac7f2e580e7541f016a1f0d2b6446e
Jin Woo Han <jin00woo@naver.com>
2023-06-16 22:28:14 +0900

    Finished screen header.

--- delivery-app/screens/RestaurantScreen.js
+++ delivery-app/screens/RestaurantScreen.js
@@ -3,9 +3,12 @@ import React, { useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { urlFor } from "../util/sanity";
 import {
-  ArrowLeftCircleIcon,
   ArrowLeftIcon,
+  ChevronRightIcon,
+  MapPinIcon,
+  StarIcon,
 } from "react-native-heroicons/solid";
+import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
 import { Colors } from "../constants/colors";
 
 const RestaurantScreen = () => {
@@ -16,6 +19,7 @@ const RestaurantScreen = () => {
       id,
       imgUrl,
       title,
+      address,
       rating,
       dishes,
       short_description,
@@ -31,7 +35,7 @@ const RestaurantScreen = () => {
     });
   }, [navigation]);
 
-  function pressBackButtonHandler(){
+  function pressBackButtonHandler() {
     navigation.goBack();
   }
 
@@ -42,9 +46,46 @@ const RestaurantScreen = () => {
           source={{ uri: urlFor(imgUrl).url() }}
           className="w-full h-56 bg-gray-300 p-4"
         />
-        <TouchableOpacity onPress={pressBackButtonHandler} className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full">
+        <TouchableOpacity
+          onPress={pressBackButtonHandler}
+          className="absolute top-14 left-5 p-2 bg-gray-100 rounded-full"
+        >
           <ArrowLeftIcon size={20} color={Colors.primary50} />
         </TouchableOpacity>
+      </View>
+
+      <View className="bg-white">
+        <View className="px-4 pt-4">
+          <Text className="text-3xl font-bold">{title}</Text>
+          <View className="flex-row my-1 space-x-2">
+            <View className="flex-row items-center space-x-1">
+              <StarIcon color={Colors.primary50} opacity={0.5} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-green-500">{rating}</Text> · {genre}
+              </Text>
+            </View>
+
+            <View className="flex-row items-center space-x-1">
+              <MapPinIcon color="gray" opacity={0.4} size={22} />
+              <Text className="text-xs text-gray-500">
+                <Text className="text-gray-500">Nearby</Text> · {address}
+              </Text>
+            </View>
+          </View>
+
+          <Text className="text-gray-500 mt-2 pb-4">{short_description}</Text>
+        </View>
+
+        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
+          <Text className="pl-2 flex-1 font-bold">
+            Have any food allergies?
+          </Text>
+          <ChevronRightIcon color={Colors.primary50} />
+        </TouchableOpacity>
+      </View>
+      <View>
+        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
       </View>
     </ScrollView>
   );
