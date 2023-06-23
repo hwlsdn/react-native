d6159da4584bbcdec401a28c6a1cd1ca1522af45
Jin Woo Han <jin00woo@naver.com>
2023-06-23 15:39:59 +0900

    Finished headerTabs component.

--- ubereats-clone/components/HeaderTabs.js
+++ ubereats-clone/components/HeaderTabs.js
@@ -1,19 +1,40 @@
 import { View, Text, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
 const HeaderButton = (props) => (
   <View>
-    <TouchableOpacity>
-      <Text>{props.text}</Text>
+    <TouchableOpacity
+      onPress={() => props.setActiveTab(props.text)}
+      className={`bg-${
+        props.activeTab === props.text ? "black" : "white"
+      } py-3 px-7 rounded-2xl`}
+    >
+      <Text
+        className={`text-${
+          props.activeTab === props.text ? "white" : "black"
+        } text-base font-semibold`}
+      >
+        {props.text}
+      </Text>
     </TouchableOpacity>
   </View>
 );
 
 const HeaderTabs = () => {
+  const [activeTab, setActiveTab] = useState("Delivery");
+
   return (
-    <View>
-      <HeaderButton text="Delivery" />
-      <HeaderButton text="Pickup" />
+    <View className="flex-row justify-evenly">
+      <HeaderButton
+        text="Delivery"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+      <HeaderButton
+        text="Pickup"
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
     </View>
   );
 };
