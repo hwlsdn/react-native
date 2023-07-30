8029064bec3f1e9814b36d72decde6979e088207
Jin Woo Han <jin00woo@naver.com>
2023-07-30 16:12:36 +0900

    Finished Categories component.

---
+++ ubereats-clone-2/components/Categories.js
@@ -0,0 +1,77 @@
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React from "react";
+
+const items = [
+  {
+    image: require("../assets/images/shopping-bag.png"),
+    text: "Pick-up",
+  },
+  {
+    image: require("../assets/images/soft-drink.png"),
+    text: "Soft Drinks",
+  },
+  {
+    image: require("../assets/images/bread.png"),
+    text: "Bakery Items",
+  },
+  {
+    image: require("../assets/images/fast-food.png"),
+    text: "Fast Foods",
+  },
+  {
+    image: require("../assets/images/deals.png"),
+    text: "Deals",
+  },
+  {
+    image: require("../assets/images/coffee.png"),
+    text: "Coffee & Tea",
+  },
+  {
+    image: require("../assets/images/desserts.png"),
+    text: "Desserts",
+  },
+];
+
+const Categories = () => {
+  return (
+    <View style={styles.container}>
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {items.map((item, index) => (
+          <TouchableOpacity
+            key={index}
+            style={{ alignItems: "center", margin: 10 }}
+          >
+            <Image style={styles.image} source={item.image} />
+            <Text style={styles.text}>{item.text}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Categories;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 5,
+    paddingVertical: 5,
+    paddingLeft: 5,
+  },
+  image: {
+    width: 50,
+    height: 40,
+    resizeMode: "contain",
+  },
+  text: {
+    fontSize: 13,
+    fontWeight: "900",
+  },
+});
