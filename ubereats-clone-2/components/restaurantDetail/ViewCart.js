e3a527bc47fa77755759e07e725950fc9922a507
Jin Woo Han <jin00woo@naver.com>
2023-08-04 15:06:21 +0900

    Used redux to cache cart elements.

--- ubereats-clone-2/components/restaurantDetail/ViewCart.js
+++ ubereats-clone-2/components/restaurantDetail/ViewCart.js
@@ -1,9 +1,14 @@
-import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
-import React, { useEffect } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Modal } from "react-native";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import OrderItem from "./OrderItem";
 
 const ViewCart = () => {
-  const items = useSelector((state) => state.cartReducer.selectedItems.items);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const { items, restaurantName } = useSelector(
+    (state) => state.cartReducer.selectedItems
+  );
 
   const total = items
     .map((item) => Number(item.price.replace("$", "")))
@@ -15,16 +20,54 @@ const ViewCart = () => {
     currency: "USD",
   });
 
+  const checkoutModalContent = () => {
+    return (
+      <>
+        <View style={styles.modalContainer}>
+          <View style={styles.modalCheckoutContainer}>
+            <Text style={styles.restaurantName}>{restaurantName}</Text>
+            {items.map((item, index) => (
+              <OrderItem key={index} item={item} />
+            ))}
+            <View style={styles.subtotalContainer}>
+              <Text style={styles.subtotalText}>Subtotal</Text>
+              <Text style={styles.subtotalText}>${totalUSD}</Text>
+            </View>
+            <View style={{ flexDirection: "row", justifyContent: "center" }}>
+              <TouchableOpacity
+                style={styles.checkoutButton}
+                onPress={() => setModalVisible(false)}
+              >
+                <Text style={styles.checkoutButtonText}>Checkout</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </>
+    );
+  };
+
   useEffect(() => {}, [total]);
 
   return (
     <>
+      <Modal
+        animationType="slide"
+        visible={modalVisible}
+        transparent={true}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        {checkoutModalContent()}
+      </Modal>
       {total !== 0 ? (
         <View style={styles.container}>
           <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.button}>
+            <TouchableOpacity
+              style={styles.button}
+              onPress={() => setModalVisible(true)}
+            >
               <Text style={{ color: "white", fontSize: 20 }}>
-                View Cart ${totalUSD}
+                View Cart - ${totalUSD}
               </Text>
             </TouchableOpacity>
           </View>
@@ -57,5 +100,46 @@ const styles = StyleSheet.create({
     padding: 13,
     borderRadius: 30,
     width: 300,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: "flex-end",
+    backgroundColor: "rgba(0,0,0,0.7)",
+  },
+  modalCheckoutContainer: {
+    backgroundColor: "white",
+    padding: 16,
+    height: 500,
+    borderWidth: 1,
+  },
+  restaurantName: {
+    textAlign: "center",
+    fontWeight: "600",
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  subtotalContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20,
+  },
+  subtotalText: {
+    textAlign: "left",
+    fontWeight: "600",
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  checkoutButton: {
+    marginTop: 20,
+    backgroundColor: "black",
+    alignItems: "center",
+    padding: 13,
+    borderRadius: 30,
+    width: 300,
+    position: "relative",
+  },
+  checkoutButtonText: {
+    color: "white",
+    fontSize: 20,
   },
 });
