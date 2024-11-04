d31752f7268085d986a501cd732508b2a307c239
jin00woo <jin00woo@naver.com>
2024-11-04 20:58:42 +0900

    FInished setting up context.

--- food-with-supabase/src/app/(tabs)/menu/[id].tsx
+++ food-with-supabase/src/app/(tabs)/menu/[id].tsx
@@ -4,17 +4,23 @@ import { useLocalSearchParams, Stack } from "expo-router";
 import products from "@/assets/data/products";
 import { defaultPizzaImage } from "@/src/components/ProductListItem";
 import Button from "@/src/components/Button";
+import { useCart } from "@/src/providers/CartProvider";
+import { PizzaSize } from "@/src/types";
 
-const sizes = ["S", "M", "L", "XL"];
+const sizes: PizzaSize[] = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-  const [selectedSize, setSelectedSize] = useState("M");
+  const [selectedSize, setSelectedSize] = useState<PizzaSize>("M");
   const { id } = useLocalSearchParams();
+  const { addItem } = useCart();
 
   const product = products.find((p) => p.id.toString() === id);
 
   const addToCart = () => {
-    console.warn("Adding to cart");
+    if (!product) {
+      return;
+    }
+    addItem(product, selectedSize);
   };
 
   if (!product) {
--- food-with-supabase/src/app/cart.tsx
+++ food-with-supabase/src/app/cart.tsx
@@ -1,10 +1,19 @@
-import { View, Text } from "react-native";
+import { View, Text, Platform, FlatList } from "react-native";
 import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { useCart } from "../providers/CartProvider";
+import CartListItem from "../components/CartListItem";
 
 const CartScreen = () => {
+  const { items } = useCart();
+
   return (
     <View>
-      <Text></Text>
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <CartListItem cartItem={item} />}
+      />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
     </View>
   );
 };
